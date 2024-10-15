1f1415f0a655b6907bb4a6d09b1381f2c045cc7b
boacentura-bit <carlos_boaventura@outlook.com.br>
2024-10-15 12:41:23 -0300

    removed script.js

--- script.js
+++
@@ -1,7 +0,0 @@
-const video = document.querySelector('video');
-
-video.addEventListener('fullscreenchange', () => {
-    if (document.fullscreenElement) {
-        document.exitFullscreen();
-    }
-});
